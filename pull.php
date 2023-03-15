1719295ec731cad2e282fbfcfcc75ace24333f7b
Bernardo Pacini <bpacini@umich.edu>
2023-03-15 19:25:31 -0400

    Attempt at fixing form validation

--- pull.php
+++ pull.php
@@ -1,5 +1,5 @@
 <?php
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 if ( $_POST['payload'] ) {
-   shell_exec('cd ~/public_html/bpaciniengineering.org && git reset --hard origin/build');
+   shell_exec('cd ~/public_html/bpaciniengineering.org && git fetch && git reset --hard origin/build');
 }
