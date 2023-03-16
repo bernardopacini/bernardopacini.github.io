85f53ecd784413c9a03f37a13d66891fcfc5225a
Bernardo Pacini <bpacini@umich.edu>
2023-03-15 23:38:44 -0400

    Fixing contact form final (hopefully) issues

--- assets/js/validateForm.js
+++ assets/js/validateForm.js
@@ -5,10 +5,11 @@ function validateForm() {
     var Mess = document.contact-form.message;
     
     var response = grecaptcha.getResponse();
-    if (Name == "" || Email == "" || Mess == "" || Org == "" || response.length == 0) {
+    if (Name === "" || Email === "" || Mess === "" || Org === "" || response.length === 0) {
         alert("Please fill all fields and check the Captcha box");
         return false;
     }
         else {setTimeout(function() { alert('Thank you!'); }, 1);
+        return true;
     }
 }
