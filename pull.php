04887df56e79746c48b97173092d9540a9849ce4
Bernardo Pacini <bpacini@umich.edu>
2023-06-08 15:24:28 -0400

    Removing contact page

--- pull.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-if ( $_POST['payload'] ) {
-   shell_exec('cd ~/public_html/bpaciniengineering.org && git fetch && git reset --hard origin/build');
-}
